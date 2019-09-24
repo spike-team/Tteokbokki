e811c12d0c4f1c8c77187c4303b3e1ef3f4c3ef1
YeYerin <parkyerin30@gmail.com>
2019-09-24 22:32:22 +0900

    Update App.js

--- bokki/src/App.js
+++ bokki/src/App.js
@@ -1,11 +1,9 @@
 import React from 'react';
 import './App.css';
-import Header from './components/Header';
 
 function App() {
   return (
-    <div className="App">
-      <Header/>
+    <div className="App">\
     </div>
   );
 }
