8b0981bae12aa8f12f02fb3fd7c0d28e72734a18
YeYerin <parkyerin30@gmail.com>
2019-10-07 12:19:41 +0900

    Register View

---
+++ bokki/src/containers/Register.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Header from '../components/Header';
+import RegisterContent from '../components/RegisterContent';
+import Footer from '../components/Footer';
+
+const Register = () => {
+    return (
+        <div>
+            <Header/>
+            <RegisterContent/>
+            <Footer/>
+        </div>
+    );
+};
+
+export default Register;
