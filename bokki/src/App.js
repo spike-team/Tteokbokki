cac9f94f0bcab468ef63bf1ea08773445fcca12d
YeYerin <parkyerin30@gmail.com>
2019-10-07 23:21:16 +0900

    Update

--- bokki/src/App.js
+++ bokki/src/App.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import './App.css';
+import Main from './containers/Main';
+import Register from './containers/Register';
 
 function App() {
   return (
     <div className="App">
+     <Main/>
     </div>
   );
 }
