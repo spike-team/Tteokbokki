5d2c8f9f3a2fbd221164ce33aa240919da55881e
YeYerin <parkyerin30@gmail.com>
2019-10-22 08:47:08 +0900

    Update 파일 재분류

--- bokki/src/components/Main/MainContent.js
+++ bokki/src/components/Main/MainContent.js
@@ -12,7 +12,6 @@ const MainContent = () => {
                 <p>StudenTable</p>
                 <span>우리 학교 시간표,<br />스튜던테이블에서 간편하게 관리하세요.</span>
             </div>
-
             
             <div className="first">
                 <span>Automatic School Timetable Generator</span>
@@ -20,19 +19,19 @@ const MainContent = () => {
                 <p>StudenTable을 사용하면 시간표 관련 업무를 자동화하여 교사 여러분의 소중한 시간을 절약할 수 있습니다.</p>
                 <div id="block">
                     <div id="one">
-                        <img className="icon" src={icon1} />
+                        <img className="icon" alt="" src={icon1} />
                         <div className="dcp"><b>시간표 자동 편성</b><br />교과 목록만 입력하면 시간표를 자동으로 생성합니다.</div>
                     </div>
                     <div id="two">
-                        <img className="icon" src={icon2} />
+                        <img className="icon" alt="" src={icon2} />
                         <div className="dcp"><b>교육행정정보시스템(나이스) 연동</b><br />시간표를 .CSV 파일로 다운받아 쉽게 연동할 수 있습니다.</div>
                     </div>
                     <div id="three">
-                        <img className="icon" src={icon3} />
+                        <img className="icon" alt="" src={icon3} />
                         <div className="dcp"><b>시간표 변동사항을 간편하게 공지</b><br />앱을 통해 학생들에게 시간표 변동사항을 공지합니다.</div>
                     </div>
                     <div id="four">
-                        <img className="icon" src={icon4} />
+                        <img className="icon" alt="" src={icon4} />
                         <div className="dcp"><b>문서 생성 자동화</b><br />공지할 때 필요한 문서를 자동으로 생성합니다.</div>
                     </div>
                 </div>
---
+++ bokki/src/containers/Login.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Header from '../components/Header/Header'
+import Footer from '../components/Footer/Footer'
+import LoginContent from '../components/Login/LoginContent';
+
+const Login = () => {
+    return (
+        <div>
+            <Header />
+            <LoginContent />
+            <Footer/>
+        </div>
+    );
+};
+
+export default Login;
