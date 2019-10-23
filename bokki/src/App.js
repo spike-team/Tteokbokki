2acc1423b5f236e06dfea6725231d33eed773729
YeYerin <parkyerin30@gmail.com>
2019-10-23 23:41:58 +0900

    Update App.js

--- bokki/src/App.js
+++ bokki/src/App.js
@@ -1,13 +1,22 @@
 import React from 'react';
 import './App.css';
+import { BrowserRouter, Route } from 'react-router-dom';
+
 import Main from './containers/Main';
+import Login from './containers/Login';
+import Auth from './containers/Auth';
 import Register from './containers/Register';
+import Meal from './containers/Meal';
 
 function App() {
   return (
-    <div className="App">
-     <Main/>
-    </div>
+    <BrowserRouter>
+      <Route path='/' component={Main} exact />
+      <Route path='/login' component={Login} />
+      <Route path='/auth' component={Auth} />
+      <Route path='/register' component={Register} />
+      <Route path='/meal' component={Meal} />
+    </BrowserRouter>
   );
 }
 
