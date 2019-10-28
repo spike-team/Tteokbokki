9d301912da113b000d83539aa92b335396f092ba
YeYerin <parkyerin30@gmail.com>
2019-10-28 23:30:07 +0900

    Update App.js
    라우터 나누기

--- bokki/src/App.js
+++ bokki/src/App.js
@@ -1,21 +1,19 @@
 import React from 'react';
 import './App.css';
 import { BrowserRouter, Route } from 'react-router-dom';
-
-import Main from './containers/Main';
-import Login from './containers/Login';
-import Auth from './containers/Auth';
-import Register from './containers/Register';
-import Meal from './containers/Meal';
+import { Main,Login,Auth,Register,Meal,seeTimetable,manageTimetable } from './containers';
 
 function App() {
   return (
     <BrowserRouter>
       <Route path='/' component={Main} exact />
+      <Route path='/main' component={Main} />
       <Route path='/login' component={Login} />
       <Route path='/auth' component={Auth} />
       <Route path='/register' component={Register} />
       <Route path='/meal' component={Meal} />
+      <Route path='/timetable' component={seeTimetable} />
+      <Route path='/manage-timetable' component={manageTimetable} />
     </BrowserRouter>
   );
 }
