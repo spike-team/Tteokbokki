2744784f4fc78948057a6b7f409b1ef1da540f0d
YeYerin <parkyerin30@gmail.com>
2019-11-12 20:07:59 +0900

    Create Timetable

---
+++ bokki/src/components/Timetable/TimetableComponent.js
@@ -0,0 +1,85 @@
+import React from 'react';
+import './Timetable.css';
+
+const TimetableComponent = () => {
+    return (
+        <div className="timetable">
+            <div className="side">
+                <p className="time-title">우리 반 시간표</p>
+                <p className="school">대덕소프트어쩌구</p>
+                <p className="class">
+                    <span>학년</span>
+                    <span>반</span>
+                </p>
+            </div>
+            <div className="table">
+            <tr>
+                <th> </th>
+                <th>월</th>
+                <th>화</th>
+                <th>수</th>
+                <th>목</th>
+                <th>금</th>
+            </tr>
+            <tr>
+                <th>1</th>
+                <th>창체<th>안현수</th> </th>
+                <th>DB<th>안현수</th> </th>
+                <th>DB<th>안현수</th> </th>
+                <th>DB<th>안현수</th> </th>
+                <th>DB<th>안현수</th> </th>
+            </tr>
+            <tr>
+                <th>2</th>
+                <th>DB<th>안현수</th> </th>
+                <th>DB<th>안현수</th> </th>
+                <th>DB<th>안현수</th> </th>
+                <th>DB<th>안현수</th> </th>
+                <th>DB<th>안현수</th> </th>
+            </tr>
+            <tr>
+                <th>3</th>
+                <th>DB<th>안현수</th> </th>
+                <th>DB<th>안현수</th> </th>
+                <th>DB<th>안현수</th> </th>
+                <th>DB<th>안현수</th> </th>
+                <th>DB<th>안현수</th> </th>
+            </tr>
+            <tr>
+                <th>4</th>
+                <th>DB<th>안현수</th> </th>
+                <th>DB<th>안현수</th> </th>
+                <th>DB<th>안현수</th> </th>
+                <th>DB<th>안현수</th> </th>
+                <th>DB<th>안현수</th> </th>
+            </tr>
+            <tr>
+                <th>5</th>
+                <th>DB<th>안현수</th> </th>
+                <th>DB<th>안현수</th> </th>
+                <th>DB<th>안현수</th> </th>
+                <th>DB<th>안현수</th> </th>
+                <th>DB<th>안현수</th> </th>
+            </tr>
+            <tr>
+                <th>6</th>
+                <th>DB<th>안현수</th> </th>
+                <th>DB<th>안현수</th> </th>
+                <th>DB<th>안현수</th> </th>
+                <th>DB<th>안현수</th> </th>
+                <th>DB<th>안현수</th> </th>
+            </tr>
+            <tr>
+                <th>7</th>
+                <th>DB<th>안현수</th> </th>
+                <th>DB<th>안현수</th> </th>
+                <th>DB<th>안현수</th> </th>
+                <th>DB<th>안현수</th> </th>
+                <th>DB<th>안현수</th> </th>
+            </tr>
+            </div> 
+        </div>
+    );
+};
+
+export default TimetableComponent;
