95df69c25085c0a2a1912f545f15efc590601a1d
YeYerin <parkyerin30@gmail.com>
2019-11-13 13:18:45 +0900

    hahahaha

--- bokki/src/containers/index.js
+++ bokki/src/containers/index.js
@@ -1,7 +1,6 @@
 export { default as Main } from './Main';
 export { default as Login } from './Login';
-export { default as Auth } from './Auth';
 export { default as Register } from './Register';
 export { default as Meal } from './Meal';
-export { default as seeTimetable } from './seeTimetable';
-export { default as manageTimetable } from './manageTimetable';
+export { default as Timetable } from './Timetable';
+export { default as ManageTimetable} from './ManageTimetable';
