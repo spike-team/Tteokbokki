33a0dd3941de4d65dfa28ab03fa8ded02908422e
YeYerin <parkyerin30@gmail.com>
2019-11-15 14:52:32 +0900

    Create Meal.js

---
+++ bokki/src/containers/Meal.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Header from '../components/Header/Header';
+import MealContent from '../components/Meal/MealContent';
+import Footer from '../components/Footer/Footer';
+
+const Meal = () => {
+    return (
+        <div>
+            <Header/>
+            <MealContent/>
+            <Footer/>
+        </div>
+    );
+};
+
+export default Meal;
