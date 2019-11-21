d1b99c00299a9e244dcb94eec1f93dc1e29c2f54
YeYerin <parkyerin30@gmail.com>
2019-11-21 20:33:02 +0900

    Update Header.js

--- bokki/src/components/Header/Header.js
+++ bokki/src/components/Header/Header.js
@@ -3,8 +3,9 @@ import './Header.css';
 import logo from '../../assets/logo.png';
 import { Link } from 'react-router-dom';
 
+
 const MenuItem = ({children}) => (
-    <div className="menu-item">{children}</div>
+    <li className="menu-item">{children}</li>
 )
 
 const Header = () => {
@@ -14,7 +15,7 @@ const Header = () => {
             <div className="menu">
                 <MenuItem><Link to='/meal'>오늘급식</Link></MenuItem>
                 <MenuItem><Link to='/timetable'>시간표 확인</Link></MenuItem>
-                <MenuItem><Link to='/manage-timetable'>시간표 관리</Link></MenuItem>
+                <MenuItem><Link to='/addtimetable'>시간표 관리</Link></MenuItem>
                 <MenuItem><Link to='/login'>로그인</Link></MenuItem>
             </div>
         </div>
